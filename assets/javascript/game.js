96f65b463fbb9bfe87295f9c902c2004945dde43
Gildon Kirk <gildonkirk@gmail.com>
2017-10-17 15:57:00 -0400

    game refactored into seperate functions, still need to add functionality for after first enemy is defeated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,37 @@
-$(document).ready(function() {
-	var luke = {
-		hp: 100,
-		attack: 15,
-		ca: 5
-	};
-	var obiWan = {
-		hp: 120,
-		attack: 8,
-		ca: 10
-	};
-	var mal = {
-		hp: 180,
-		attack: 5,
-		ca: 25
-	};
-	var sidious = {
-		hp: 150,
-		attack: 5,
-		ca: 20
-	};
-	var characterChoice = '';
-	var defenderChoice = '';
-	var heroHealth = '';
-	var villainHealth = ''
+var luke = {
+	hp: 100,
+	attack: 15,
+	ca: 5
+};
+var obiWan = {
+	hp: 120,
+	attack: 8,
+	ca: 10
+};
+var mal = {
+	hp: 180,
+	attack: 5,
+	ca: 25
+};
+var sidious = {
+	hp: 150,
+	attack: 5,
+	ca: 20
+};
+var characterChoice = '';
+var defenderChoice = '';
+var caPower = 0;
+var attackPower = 0;
+var heroHealth = 0;
+var villainHealth = 0;
+var characterChosen = false;
+var enemyChosen = false;
+characterChoosing();
+enemyChoosing();
+battle();
 
+function characterChoosing() {
+	$('.attkBtn').hide();
 	$('.char').on('click', function (){
 		var characterChoice = $(this);
 		$(this).appendTo('.yourChar');
@@ -32,22 +40,38 @@ $(document).ready(function() {
 		$('.charSelect').appendTo('.enemies');
 
 		if (characterChoice.is('#luke')) {
+			$('#obiWan').addClass('villain');
+			$('#mal').addClass('villain');
+			$('#sidious').addClass('villain');
 			var characterChoice = luke;
 		} else if (characterChoice.is('#obiWan')) {
+			$('#luke').addClass('villain');
+			$('#mal').addClass('villain');
+			$('#sidious').addClass('villain');
 			var characterChoice = obiWan;
 		} else if (characterChoice.is('#mal')) {
+			$('#obiWan').addClass('villain');
+			$('#luke').addClass('villain');
+			$('#sidious').addClass('villain');
 			var characterChoice = mal;
 		} else {
+			$('#obiWan').addClass('villain');
+			$('#mal').addClass('villain');
+			$('#luke').addClass('villain');
 			var characterChoice = sidious;
 		};
-		console.log(characterChoice.hp);
+		attackPower = characterChoice.attack;
+		heroHealth = characterChoice.hp;
+		console.log(`Hero HP: ${heroHealth}`);
 		$('.char').off();
+	})
+};
 
-		var attackPower = characterChoice.attack
-		$('.enemies').on('click', '.avail', function(){
+function enemyChoosing() {
+	$(document).on('click', '.villain', function(){
 		var defenderChoice = $(this);
 		$(this).appendTo('.defender');
-		$(this).addClass('villain');
+		$(this).addClass('villainToAttack');
 		if (defenderChoice.is('#luke')) {
 			var defenderChoice = luke;
 		} else if (defenderChoice.is('#obiWan')) {
@@ -57,31 +81,22 @@ $(document).ready(function() {
 		} else {
 			var defenderChoice = sidious;
 		};
-		console.log(defenderChoice.hp);
-
-			
-			$('.attkBtn').on('click', function (){
-				
-				if(defenderChoice.hp > 0 && characterChoice.hp > 0){
-					defenderChoice.hp = defenderChoice.hp - characterChoice.attack;
-					console.log(defenderChoice.hp);
-					characterChoice.hp = characterChoice.hp - defenderChoice.ca;
-					console.log(characterChoice.hp);
-					characterChoice.attack = characterChoice.attack + attackPower;
-
-					if (defenderChoice.hp < 1){
-						$('.villain').remove();
-					} 
-
-					if (characterChoice.hp < 1) {
-						$('.fightSection').after('<p>You Lose!</p>');
-					}
-				}
-			});
-			// heroHealth = characterChoice.hp
-			// $('.heroHealth').html('Hero Health: ' + heroHealth)
-			// villainHealth = defenderChoice.hp
-			// $('villainHealth').html('Villain Health: ' + villainHealth)
-		});
+		caPower = defenderChoice.ca;
+		villainHealth = defenderChoice.hp;
+		console.log(`Enemy HP: ${villainHealth}`);
+		$('.attkBtn').show();
 	});
-});
+};
+
+function battle() {
+	$('.attkBtn').on('click', function (){
+		if(villainHealth > 0 && heroHealth > 0){
+			villainHealth = villainHealth - attackPower;
+			console.log(`Enemy HP: ${villainHealth}`);
+			heroHealth = heroHealth - caPower;
+			console.log(`Hero HP: ${heroHealth}`);
+			attackPower = attackPower + attackPower;
+			console.log(`Attack Power: ${attackPower}`);
+		}
+	});
+};
