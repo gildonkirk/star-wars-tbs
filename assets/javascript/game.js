d63419000b22cdea02fdc7f4c1f1add6a1bb9a64
Gildon Kirk <gildonkirk@gmail.com>
2017-11-05 15:17:29 -0500

    game restarts successfully

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -43,21 +43,25 @@ function characterChoosing() {
 			$('#obiWan').addClass('villain').appendTo('.enemies');
 			$('#mal').addClass('villain').appendTo('.enemies');
 			$('#sidious').addClass('villain').appendTo('.enemies');
+			$('.heroNameTitle').text('Luke');
 			var characterChoice = luke;
 		} else if (characterChoice.is('#obiWan')) {
 			$('#luke').addClass('villain').appendTo('.enemies');
 			$('#mal').addClass('villain').appendTo('.enemies');
 			$('#sidious').addClass('villain').appendTo('.enemies');
+			$('.heroNameTitle').text('Obi Wan');
 			var characterChoice = obiWan;
 		} else if (characterChoice.is('#mal')) {
 			$('#obiWan').addClass('villain').appendTo('.enemies');
 			$('#luke').addClass('villain').appendTo('.enemies');
 			$('#sidious').addClass('villain').appendTo('.enemies');
+			$('.heroNameTitle').text('Darth Mal');
 			var characterChoice = mal;
 		} else {
 			$('#obiWan').addClass('villain').appendTo('.enemies');
 			$('#mal').addClass('villain').appendTo('.enemies');
 			$('#luke').addClass('villain').appendTo('.enemies');
+			$('.heroNameTitle').text('Darth Sidious');
 			var characterChoice = sidious;
 		};
 		attackPower = characterChoice.attack;
@@ -73,21 +77,25 @@ function enemyChoosing() {
 		$(defenderChoice).addClass('villainToAttack');
 		if (defenderChoice.is('#luke')) {
 			var defenderChoice = luke;
+			$('.villainNameTitle').text('Luke');
 			$('#obiWan').removeClass('villainToAttack');
 			$('#mal').removeClass('villainToAttack');
 			$('#sidious').removeClass('villainToAttack');
 		} else if (defenderChoice.is('#obiWan')) {
 			var defenderChoice = obiWan;
+			$('.villainNameTitle').text('Obi Wan');
 			$('#luke').removeClass('villainToAttack');
 			$('#mal').removeClass('villainToAttack');
 			$('#sidious').removeClass('villainToAttack');
 		} else if (defenderChoice.is('#mal')) {
 			var defenderChoice = mal;
+			$('.villainNameTitle').text('Darth Mal');
 			$('#obiWan').removeClass('villainToAttack');
 			$('#luke').removeClass('villainToAttack');
 			$('#sidious').removeClass('villainToAttack');
 		} else {
 			var defenderChoice = sidious;
+			$('.villainNameTitle').text('Darth Sidious');
 			$('#obiWan').removeClass('villainToAttack');
 			$('#mal').removeClass('villainToAttack');
 			$('#luke').removeClass('villainToAttack');
@@ -115,7 +123,7 @@ function battle() {
 					heroHealth = 0;
 				}
 				$('.heroScore').html(`${heroHealth}`);
-				// gameOverLose();
+				gameOverLose();
 			}
 			attackPower = attackPower + attackPower;
 			$('.heroAttack').html(`${attackPower}`);
@@ -128,7 +136,7 @@ function villainDead() {
 		$('.villainToAttack').hide();
 		$('.villainToAttack').removeClass('.villainToAttack');
 		enemiesBeaten++;
-		// gameOverWin();
+		gameOverWin();
 		enemyChoosing();
 	}
 }
@@ -137,7 +145,7 @@ function gameOverLose() {
 	if(villainHealth > 0 && heroHealth <= 0){
 		$('.gameContainer').after('<div class="gameOverModal youLose"><p class="gameOverText">You Lose!</p><button class="gameOverButton">Play Again?</button></div>');
 		$('.gameContainer').hide();
-		// restartGame();
+		restartGame();
 	}
 };
 
@@ -145,13 +153,23 @@ function gameOverWin() {
 	if(heroHealth > 0 && villainHealth <= 0 && enemiesBeaten === 3){
 		$('.gameContainer').after('<div class="gameOverModal youWin"><p class="gameOverText">You Win!</p><button class="gameOverButton">Play Again?</button></div>');
 		$('.gameContainer').hide();
-		// restartGame();
+		restartGame();
 	}
 };
 
 function restartGame() {
 	$('.gameOverButton').on('click', function () {
-		$('.charSelect').append('.villain');
+		$('.gameOverModal').hide();
 		$('.gameContainer').show();
+		$('.char').show();
+		$('.charSelect').append($('.char'));
+		$('.char').removeClass('hero').removeClass('villain').removeClass('villainToAttack');
+		$('.char').addClass('avail');
+		$('.villainNameTitle').text('Villain');
+		$('.heroNameTitle').text('Hero');
+		$('.heroScore').text(0);
+		$('.heroAttack').text(0);
+		$('.villainScore').text(0);
+		$('.villainAttack').text(0);
 	})
 }
